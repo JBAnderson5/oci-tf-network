8f339e4b30ffc6ef589c133ee6acc25eb8cb79e8
JB Anderson <35785660+JBAnderson5@users.noreply.github.com>
2023-11-06 15:25:50 -0800

    trying another syntax

--- subnet/vcn-data.tf
+++ subnet/vcn-data.tf
@@ -91,7 +91,7 @@ data "oci_core_services" "this" {
 
   filter {
     name   = "id"
-    values = var.service_gateway.services.0.service_id
+    values = tolist(var.service_gateway.services).service_id
     # regex  = true
   }
 
